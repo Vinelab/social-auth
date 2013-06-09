a08159058a53f7f752458d3ecbaa134f2a946a20
Abed Halawi <halawi.abed@gmail.com>
2013-06-10 01:39:35 +0300

    Update routes - introduce 'auth' route group

--- src/routes.php
+++ src/routes.php
@@ -4,9 +4,7 @@ Route::group(['prefix' => 'auth'], function(){
 
 	Route::group(['prefix' => 'social'], function(){
 
-		Route::get('{service}', function($service){
-
-		});
-
+		Route::get('{service}', 'Vinelab\Auth\Controllers\AuthenticationController@index');
+		Route::get('{service}/callback', 'Vinelab\Auth\Controllers\AuthenticationController@callback');
 	});
 });
