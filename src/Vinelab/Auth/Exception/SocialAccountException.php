b08b35983589be74a8559e6e751b43bb4524996f
Abed Halawi <halawi.abed@gmail.com>
2013-06-14 15:56:28 +0300

    Add SocialAccountException

---
+++ src/Vinelab/Auth/Exception/SocialAccountException.php
@@ -0,0 +1,9 @@
+<?php namespace Vinelab\Auth\Exception;
+
+Class SocialAccountException extends \Exception {
+
+	function __construct($error, $message, $code = 2, Exception $previous = null)
+	{
+		parent::__construct(sprintf('Social Account (Profile) Error: %s - %s', $error, $message), $code, $previous);
+	}
+}
