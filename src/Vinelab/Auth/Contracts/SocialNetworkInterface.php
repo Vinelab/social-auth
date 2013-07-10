258e3612bfa2c5746999cd18c9ac51bceffc5168
Abed Halawi <halawi.abed@gmail.com>
2013-07-10 12:08:24 +0300

    Add documentation to SocialNetworkInterface

--- src/Vinelab/Auth/Contracts/SocialNetworkInterface.php
+++ src/Vinelab/Auth/Contracts/SocialNetworkInterface.php
@@ -2,9 +2,34 @@
 
 interface SocialNetworkInterface {
 
+	/**
+	 * Generate and return the authentication URL
+	 *
+	 * @return string
+	 */
 	public function authenticationURL();
+
+	/**
+	 * Return the settings or a single setting
+	 *
+	 * @param  string $setting
+	 * @return mixed
+	 */
 	public function settings($setting = null);
+
+	/**
+	 * Handle the authentication callback when received
+	 *
+	 * @param  mixed $input
+	 * @return mixed
+	 */
 	public function authenticationCallback($input);
+
+	/**
+	 * Return the authenticated user's profile
+	 *
+	 * @return mixed
+	 */
 	public function profile();
 
 }
