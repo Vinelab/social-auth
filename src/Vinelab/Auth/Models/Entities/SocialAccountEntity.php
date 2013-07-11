df827f5edda789d70d17a2ad356d27a9f76f9fbe
Abed Halawi <halawi.abed@gmail.com>
2013-07-11 10:36:52 +0300

    Add EloquentObserver,  and  (false) to SocialAccountEntity

--- src/Vinelab/Auth/Models/Entities/SocialAccountEntity.php
+++ src/Vinelab/Auth/Models/Entities/SocialAccountEntity.php
@@ -2,8 +2,15 @@
 
 use Illuminate\Database\Eloquent\Model;
 
+use Vinelab\Auth\Models\Observers\EloquentObserver;
+
 class SocialAccountEntity extends Model {
 
 	protected $table = 'social_accounts';
+
+	protected $fillable = ['network', 'account_id', 'user_id', 'access_token'];
+
+	public $incrementing = false;
 }
 
+SocialAccountEntity::observe(new EloquentObserver);
