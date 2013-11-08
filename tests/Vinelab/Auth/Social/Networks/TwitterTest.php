1bc9e6010f1f9cf2065796ca5251d09d0f26c088
Abed Halawi <halawi.abed@gmail.com>
2013-11-08 15:38:55 +0200

    Fix type-os in tests

--- tests/Vinelab/Auth/Social/Networks/TwitterTest.php
+++ tests/Vinelab/Auth/Social/Networks/TwitterTest.php
@@ -1,7 +1,7 @@
 <?php namespace Vinelab\Auth\Tests;
 
-use PHPUnit_Framework_TestaCase as TestaCase;
-use Mocker as M;
+use PHPUnit_Framework_TestCase as TestCase;
+use Mockery as M;
 
 use Vinelab\Auth\Social\Networks\Twitter as Twitter;
 
@@ -12,8 +12,26 @@ class TwitterTest extends TestCase {
         $this->samplesDir = './tests/samples';
 
         $this->settings = [
-
+            'base_url'     => 'http://localhost/twitter',
+            'key'          => 'somekey-twitter',
+            'secret'       => 'some-secret-key-twitter',
+            'redirect_uri' => 'http://localhost/twitter/callback'
         ];
+
+        $this->mConfig = M::mock('Illuminate\Config\Repository');
+        $this->mConfig->shouldReceive('get')
+            ->once('auth::social.twitter')
+            ->andReturn($this->settings);
+
+        $this->mResponse = M::mock('Vinelab\Http\Response');
+        $this->mHttpClient = M::mock('Vinelab\Http\Client[get]');
+    }
+
+    public function testAuthentication()
+    {
+        $t = new Twitter($this->mConfig, $this->mHttpClient);
+        $this->assertTrue(method_exists($t, 'authenticate'));
+        $this->assertTrue(method_exists($t, 'authenticationCallback'));
     }
 
 }
